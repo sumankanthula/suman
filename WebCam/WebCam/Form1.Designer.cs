ed010ded1d40339d3c60049c6f09d03378ea4c30
Sai Kiran Mandhala <suman.kanthula@live.com>
2021-06-07 10:48:13 +0530

    teslong otoscope features
    Co-Authored-By: sumankanthula <5969849+sumankanthula@users.noreply.github.com>

---
+++ WebCam/WebCam/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace WebCam
+{
+    partial class webCamForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCamera = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbCamera = new System.Windows.Forms.PictureBox();
+            this.btnCameraStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.Reload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbCamera
+            // 
+            this.cmbCamera.FormattingEnabled = true;
+            this.cmbCamera.Location = new System.Drawing.Point(204, 12);
+            this.cmbCamera.Name = "cmbCamera";
+            this.cmbCamera.Size = new System.Drawing.Size(298, 28);
+            this.cmbCamera.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera";
+            // 
+            // pbCamera
+            // 
+            this.pbCamera.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCamera.Location = new System.Drawing.Point(12, 68);
+            this.pbCamera.Name = "pbCamera";
+            this.pbCamera.Size = new System.Drawing.Size(863, 392);
+            this.pbCamera.TabIndex = 2;
+            this.pbCamera.TabStop = false;
+            // 
+            // btnCameraStart
+            // 
+            this.btnCameraStart.Location = new System.Drawing.Point(437, 466);
+            this.btnCameraStart.Name = "btnCameraStart";
+            this.btnCameraStart.Size = new System.Drawing.Size(94, 29);
+            this.btnCameraStart.TabIndex = 3;
+            this.btnCameraStart.Text = "Start";
+            this.btnCameraStart.UseVisualStyleBackColor = true;
+            this.btnCameraStart.Click += new System.EventHandler(this.btnCameraStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(273, 466);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 29);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Reload
+            // 
+            this.Reload.Location = new System.Drawing.Point(580, 12);
+            this.Reload.Name = "Reload";
+            this.Reload.Size = new System.Drawing.Size(94, 29);
+            this.Reload.TabIndex = 5;
+            this.Reload.Text = "btnReload";
+            this.Reload.UseVisualStyleBackColor = true;
+            this.Reload.Click += new System.EventHandler(this.Reload_Click);
+            // 
+            // webCamForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 615);
+            this.Controls.Add(this.Reload);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnCameraStart);
+            this.Controls.Add(this.pbCamera);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCamera);
+            this.Name = "webCamForm";
+            this.Text = "Web Cam";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.webCamForm_FormClosing);
+            this.Load += new System.EventHandler(this.webCamForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCamera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pbCamera;
+        private System.Windows.Forms.Button btnCameraStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button Reload;
+    }
+}
+
---
+++ WebCam/WebCam/Form1.cs
@@ -0,0 +1,92 @@
+﻿using AForge.Imaging.Filters;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WebCam
+{
+    public partial class webCamForm : Form
+    {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        public webCamForm()
+        {
+            InitializeComponent();
+        }
+
+        private void webCamForm_Load(object sender, EventArgs e)
+        {
+            ReloadNewDevices();
+            LoadDefaultImage();
+        }
+
+        private void ReloadNewDevices()
+        {
+            cmbCamera.Items.Clear();
+
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in filterInfoCollection)
+            {
+                cmbCamera.Items.Add(item.Name);
+            }
+            cmbCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void btnCameraStart_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+            {
+                MessageBox.Show(string.Format("Camera:{0}  is already running", cmbCamera.SelectedItem));
+                return;
+            }
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cmbCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            var bitmap = (Bitmap)eventArgs.Frame.Clone();
+            var filter = new Mirror(false, true);
+            filter.ApplyInPlace(bitmap);
+            pbCamera.Image = bitmap;
+        }
+
+        private void webCamForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+            {
+                videoCaptureDevice.Stop();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (!videoCaptureDevice.IsRunning)
+            {
+                MessageBox.Show("Camera is not running");
+                return;
+            }
+
+            //videoCaptureDevice.Stop();
+            videoCaptureDevice.SignalToStop();
+            videoCaptureDevice.NewFrame -= VideoCaptureDevice_NewFrame;
+            //videoCaptureDevice = null;
+            //Thread.Sleep(1000);
+            LoadDefaultImage();
+        }
+
+        private void LoadDefaultImage()
+        {
+            var bitMap = new Bitmap(@"C:\Users\Suman\source\repos\WebCam\WebCam\camera_btn.jpg");
+            pbCamera.Image = bitMap;
+        }
+        private void Reload_Click(object sender, EventArgs e)
+        {
+            ReloadNewDevices();
+        }
+    }
+}
